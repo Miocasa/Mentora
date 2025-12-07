2e3d9cb2e0ab6486cab29ac46ea939a19e6c0007
IV <nyessensary@gmail.com>
2025-12-07 15:39:55 +0500

    course_detail_screen.dart / l10n.yaml изменилось

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,9 @@ plugins {
 
 android {
     namespace = "com.example.course"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
+    buildToolsVersion = "36.1.0" 
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -28,7 +29,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
